5ea93bf943de6f85fdb164165c4248fb9ab9460e
Kriss-Hanna <kristofferson.hanna@gmail.com>
2021-06-18 06:10:47 +0200

    first commit

---
+++ README.txt
@@ -0,0 +1,4 @@
+Utilisation de l'api randomuser 
+Gestion de l'asynchrone avec async/await
+Injection dans le dom depuis le script.js
+Traitement de la date timestamp pour un rendu plus agréable
---
+++ script.js
@@ -0,0 +1,52 @@
+// https://randomuser.me/api/?results=24
+
+let userData = [];
+
+async function fetchUser(){
+
+await fetch("https://randomuser.me/api/?results=24")
+.then((res) => res.json())
+.then((data) => userData = data.results)
+}
+
+const userDisplay = async () => {
+
+  await fetchUser();
+
+  const dateParser = (date) => {
+     let newDate = new Date(date).toLocaleDateString("fr-FR", {
+       year: "numeric",
+       month: "long",
+       day: "numeric"
+     })
+     return newDate
+  };
+
+  const dateCalc = (date) => {
+
+    let today = new Date();
+    let todayTimestamp = Date.parse(today)
+    let timestamp = Date.parse(date)
+
+    return Math.ceil((todayTimestamp - timestamp) / 8.64e7)
+
+  };
+
+  document.body.innerHTML = userData.map((user) => 
+
+  `
+  <div class="card">
+    <img src=${user.picture.large} alt ="photo de ${user.name.last}"/>
+    <h3>${user.name.first}</h3>
+    <h4>${user.name.last}</h4>
+    <p>${user.location.city}, ${dateParser(user.dob.date)}</p>
+    <em>Membre depuis : ${dateCalc(user.registered.date)} jours</em>
+  </div>
+
+  `
+  )
+  .join("")
+}
+
+userDisplay();
+
